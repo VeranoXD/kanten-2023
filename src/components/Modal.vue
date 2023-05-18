09c2ffcd329bff25ba47c9889e190e309b235bb8
VeranoXD <45197145+VeranoXD@users.noreply.github.com>
2023-05-18 19:36:24 +0200

    PopUp box

---
+++ src/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="modal flex justify-center items-center z-50 absolute w-full h-full top-0 bg-black bg-opacity-90">
+        <div class="modal-content flex flex-col justify-center rounded-lg w-80 py-10 px-8 bg-gray-200">
+            <p class="text-center">{{ this.modalMessage }}</p>
+            <button class=" self-center text-white bg-black rounded-full px-10 py-3 w-full max-w-sm" @click="closeModal">Luk</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["modalMessage"],
+    methods: {
+        closeModal() {
+            this.$emit("close-modal");
+        },
+    },
+};
+</script>
--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="reset-password">
+    <Modal v-if="modalActive" v-on:close-modal="closeModal"/>
     <div class="form-wrap overflow-hidden flex justify-center h-screen self-center my-0 mx-auto w-full bg-black">
       <form class="reset py-0 px-2 relative flex flex-col justify-center items-center flex-1">
         <h2 class="text-white font-extrabold text-3xl">Nulstil Adgangskode</h2>
@@ -41,8 +42,23 @@
 </template>
 
 <script>
+import Modal from '../components/Modal.vue';
 export default {
+  components: { Modal },
   name: "GlemtAdgangskode",
+  data(){
+    return {
+        email: null,
+        modalActive: true,
+        modalMessage: ""
+    }
+  },
+  methods: {
+    closeModal() {
+       this.modalActive = !this.modalActive;
+       this.email = "";
+    }
+  }
 };
 </script>
 
